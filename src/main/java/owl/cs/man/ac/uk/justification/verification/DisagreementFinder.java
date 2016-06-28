9b36daf25678c4be36c0b0add68fb0897f06ac78
mbaxkml3 <michael.lee-2@manchester.ac.uk>
2016-06-28 11:23:38 +0100

    Clean disagreement finder. Now needs experimental framework.

--- src/main/java/owl/cs/man/ac/uk/justification/verification/DisagreementFinder.java
+++ src/main/java/owl/cs/man/ac/uk/justification/verification/DisagreementFinder.java
@@ -2,7 +2,9 @@ package owl.cs.man.ac.uk.justification.verification;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
 import org.semanticweb.owlapi.apibinding.OWLManager;
@@ -13,6 +15,7 @@ import org.semanticweb.owlapi.model.OWLOntology;
 import org.semanticweb.owlapi.model.OWLOntologyCreationException;
 import org.semanticweb.owlapi.model.OWLOntologyManager;
 import org.semanticweb.owlapi.model.OWLOntologyStorageException;
+import org.semanticweb.owlapi.model.OWLSubClassOfAxiom;
 
 public class DisagreementFinder {
 
@@ -29,104 +32,147 @@ public class DisagreementFinder {
 		this.ontoman = OWLManager.createOWLOntologyManager();
 	}
 	
-	public void getDifferences(ArrayList<String> fileList, String savePath){
+	public ArrayList<String> getDifferences(ArrayList<String> fileList, String savePath, OWLOntology original){
+		ArrayList<String> cleanedList = this.cleanList(fileList);
+		Set<OWLAxiom> differenceSet = this.getAxioms(new File(cleanedList.get(0)));
+		Set<OWLAxiom> saveSet = new HashSet<OWLAxiom>(differenceSet);
+		Map<String,Set<OWLAxiom>> collection = new HashMap<String,Set<OWLAxiom>>();
+		collection.put(cleanedList.get(0).substring(cleanedList.get(0).lastIndexOf("/") + 1), saveSet);
+		cleanedList.remove(0);
+		Set<OWLAxiom> unionSet = new HashSet<OWLAxiom>(differenceSet);
+		for(String path:fileList)
+		{
+			Set<OWLAxiom> returnSet = this.getAxioms(new File(path));
+			unionSet.addAll(returnSet);
+			//If returnSet is empty, an error occured. We skip this.
+			if(!returnSet.isEmpty())
+			{
+				differenceSet.retainAll(returnSet);
+				collection.put(path.substring(path.lastIndexOf("/") + 1), returnSet);
+			}
+		}
+		unionSet.removeAll(differenceSet);
+		Set<OWLAxiom> selfjust = this.getAssertedAxioms(original, unionSet);
+		unionSet.removeAll(selfjust);
+		System.out.println(unionSet.size());
+		this.saveDifferences(unionSet, savePath);
+		return this.getSJList(collection, selfjust);
+	}
+
+	
+	//Removes any funky ontologies (empties, broken)
+	//Will produce a flag to be caught
+	private ArrayList<String> cleanList(ArrayList<String> fileList){
+		ArrayList<String> cleanedList = new ArrayList<String>(fileList);
+		for(String path:fileList)
+		{
+			File loadingFile = new File(path);
+			if(!loadingFile.exists() || loadingFile.length() == 0)
+			{
+				cleanedList.remove(path);
+				System.out.println("Error with: " + path);
+			}
+		}
+		return cleanedList;
+	}
+	
+	//Gets failures and success for selfjusts 
+	//and puts them in arraylist
+	private ArrayList<String> getSJList(Map<String,Set<OWLAxiom>> collection,Set<OWLAxiom> disagreements){
+		ArrayList<String> details = new ArrayList<String>();
+		for(OWLAxiom ax:disagreements)
+		{
+			String detail = ax.toString();
+			String yay = "YES: ";
+			String nay = "NO: ";
+			for(String s:collection.keySet())
+			{
+				if(collection.get(s).contains(ax))
+				{
+					yay = yay + s + " ";
+				}
+				else
+				{
+					nay = nay + s + " ";
+				}
+			}
+			detail = detail + " " + yay + nay;
+			details.add(detail);
+		}
+		return details;
+	}
+	
+	//Gets all axioms originally asserted in the ontology
+	private Set<OWLAxiom> getAssertedAxioms(OWLOntology ontology, Set<OWLAxiom> disagreements){
+		Set<OWLAxiom> selfjust = new HashSet<OWLAxiom>();
+		for(OWLAxiom ax:disagreements)
+		{
+			if(ontology.containsAxiom(ax))
+			{
+				selfjust.add(ax);
+			}
+		}
+		return selfjust;
+	}
+	
+	//Loads ontology from file, gets axioms then removes ontology.
+	//Spits out empty set in case ontology loading fails, is
+	//empty or is unavailable.
+	private Set<OWLAxiom> getAxioms(File file){
+		Set<OWLAxiom> returnSet = new HashSet<OWLAxiom>();
 		try {
-			OWLOntology checkOntology = ontoman.loadOntologyFromOntologyDocument(new File(fileList.get(0)));
-			fileList.remove(0);
-			Set<OWLAxiom> differenceSet = checkOntology.getAxioms();
-			ontoman.removeOntology(checkOntology);
-			Set<OWLAxiom> unionSet = new HashSet<OWLAxiom>();
-			for(String path:fileList)
-			{
-				OWLOntology checkOntology2 = ontoman.loadOntologyFromOntologyDocument(new File(path));
-				//if appears totally fucked, ignore
-				unionSet.addAll(checkOntology2.getAxioms());
-				differenceSet.retainAll(checkOntology2.getAxioms());
-				ontoman.removeOntology(checkOntology2);
+			if(file.exists() && file.length() != 0){
+				OWLOntology onto = ontoman.loadOntologyFromOntologyDocument(file);
+				returnSet.addAll(onto.getAxioms());
+				ontoman.removeOntology(onto);
 			}
-			unionSet.removeAll(differenceSet);
-			int i = 0;
-			for(OWLAxiom ax:unionSet)
-			{
-				if(!ax.isOfType(AxiomType.DECLARATION))
-				{
-					System.out.println(ax);
-					i++;
-				}
-			}
-			System.out.println(i);
-			OWLOntology diffOntology = ontoman.createOntology(unionSet);
-			File saveFile = new File(savePath);
-			ontoman.saveOntology(diffOntology, IRI.create(saveFile));
-			/**ontoman.removeOntology(checkOntology);
-			for(String path:fileList)
-			{
-				OWLOntology checkOntology2 = ontoman.loadOntologyFromOntologyDocument(new File(path));
-				//if appears totally fucked, ignore
-				differenceSet.addAll(this.setDifference(checkSet, checkOntology2.getAxioms()));
-				ontoman.removeOntology(checkOntology2);
-			}
-			int i = 0;
-			for(OWLAxiom ax:differenceSet)
-			{
-				if(!ax.isOfType(AxiomType.DECLARATION))
-				{
-					System.out.println(ax);
-					i++;
-				}
-			}
-			System.out.println(i);
-			OWLOntology diffOntology = ontoman.createOntology(differenceSet);
-			File saveFile = new File(savePath);
-			ontoman.saveOntology(diffOntology, IRI.create(saveFile));
-			**/
 		} catch (OWLOntologyCreationException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		} catch (OWLOntologyStorageException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
 		}
+		return returnSet;
 	}
 	
-	
-	public Set<OWLAxiom> setDifference(Set<OWLAxiom> set1, Set<OWLAxiom> set2){
-		//Set<OWLAxiom> diffSet = new HashSet<OWLAxiom>();
-		Set<OWLAxiom> union = new HashSet<OWLAxiom>();
-		Set<OWLAxiom> intersection = new HashSet<OWLAxiom>();
-		union.addAll(set1);
-		union.addAll(set2);
-		set1.retainAll(set2);
-		intersection.addAll(set1);
-		union.removeAll(intersection);
-		return union;
-		/**
-		Set<OWLAxiom> bigSet;
-		Set<OWLAxiom> smallSet;
-		if(set1.size() > set2.size())
+	private void saveDifferences(Set<OWLAxiom> differences, String savePath){
+		//Filtering of axioms - remove declarations and tautologies. At the moment tautologies are
+		//just defined w.r.t classification. Later versions will require something more complex.
+		Set<OWLAxiom> filter = new HashSet<OWLAxiom>();
+		for(OWLAxiom ax:differences)
 		{
-			bigSet = set1;
-			smallSet = set2;
-		}
-		else
-		{
-			bigSet = set2;
-			smallSet = set1;
-		}
-		for(OWLAxiom ax:bigSet)
-		{
-			if(!smallSet.contains(ax))
+			if(ax.isOfType(AxiomType.DECLARATION))
 			{
-				diffSet.add(ax);
+				filter.add(ax);
 			}
 			else
-			{
-				smallSet.remove(ax);
+			{	
+				//Sanity check
+				if(ax.isOfType(AxiomType.SUBCLASS_OF))
+				{
+					if(this.isTautology((OWLSubClassOfAxiom) ax))
+					{
+						filter.add(ax);
+					}
+				}
 			}
 		}
-		diffSet.addAll(smallSet);
-		return diffSet;
-		**/
+		differences.removeAll(filter);
+		try {
+			OWLOntology diffOntology = ontoman.createOntology(differences);
+			File saveFile = new File(savePath);
+			try {
+				ontoman.saveOntology(diffOntology, IRI.create(saveFile));
+			} catch (OWLOntologyStorageException e) {
+				System.out.println("Failed to save OWLOntology at " + savePath);
+				e.printStackTrace();
+			}
+		} catch (OWLOntologyCreationException e) {
+			System.out.println("Failed to create OWLOntology on difference set.");
+			e.printStackTrace();
+		}		
 	}
 	
+	private Boolean isTautology(OWLSubClassOfAxiom ax){
+		return ax.getSubClass().isOWLNothing() || ax.getSuperClass().isOWLThing();
+	}
+		
 }
--- src/main/java/owl/cs/man/ac/uk/justification/verification/DisagreementFinderRunner.java
+++ src/main/java/owl/cs/man/ac/uk/justification/verification/DisagreementFinderRunner.java
@@ -1,5 +1,6 @@
 package owl.cs.man.ac.uk.justification.verification;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Set;
@@ -8,6 +9,8 @@ import org.semanticweb.owlapi.apibinding.OWLManager;
 import org.semanticweb.owlapi.model.IRI;
 import org.semanticweb.owlapi.model.OWLAxiom;
 import org.semanticweb.owlapi.model.OWLDataFactory;
+import org.semanticweb.owlapi.model.OWLOntology;
+import org.semanticweb.owlapi.model.OWLOntologyCreationException;
 import org.semanticweb.owlapi.model.OWLOntologyManager;
 
 public class DisagreementFinderRunner {
@@ -15,13 +18,25 @@ public class DisagreementFinderRunner {
 	public static void main(String[] args) {
 		DisagreementFinder disf = new DisagreementFinder();
 		ArrayList<String> paths = new ArrayList<String>();
-		String path = "/home/michael/experiments/spot_tests/reasoner_verification_test/test_3/cao.disagreements.owl";
-		paths.add("/home/michael/experiments/spot_tests/reasoner_verification_test/test_3/inf_fact_cao.clusters-of-orthologous-groups-cog-analysis-ontology.3.orig.owl");
-		paths.add("/home/michael/experiments/spot_tests/reasoner_verification_test/test_3/inf_hermit_cao.clusters-of-orthologous-groups-cog-analysis-ontology.3.orig.owl");
-		paths.add("/home/michael/experiments/spot_tests/reasoner_verification_test/test_3/inf_jfact_cao.clusters-of-orthologous-groups-cog-analysis-ontology.3.orig.owl");
-		paths.add("/home/michael/experiments/spot_tests/reasoner_verification_test/test_3/inf_more-hermit_cao.clusters-of-orthologous-groups-cog-analysis-ontology.3.orig.owl");
-		paths.add("/home/michael/experiments/spot_tests/reasoner_verification_test/test_3/inf_pellet_cao.clusters-of-orthologous-groups-cog-analysis-ontology.3.orig.owl");
-		disf.getDifferences(paths, path);
+		String path = "/home/michael/experiments/spot_tests/disagreement_finder/disagreement_with_sj_cao/cao_wo_taut.disagreements.owl";
+		paths.add("/home/michael/experiments/spot_tests/disagreement_finder/disagreement_with_sj_cao/inf_fact_cao.clusters-of-orthologous-groups-cog-analysis-ontology.3.orig.owl");
+		paths.add("/home/michael/experiments/spot_tests/disagreement_finder/disagreement_with_sj_cao/inf_hermit_cao.clusters-of-orthologous-groups-cog-analysis-ontology.3.orig.owl");
+		paths.add("/home/michael/experiments/spot_tests/disagreement_finder/disagreement_with_sj_cao/inf_jfact_cao.clusters-of-orthologous-groups-cog-analysis-ontology.3.orig.owl");
+		paths.add("/home/michael/experiments/spot_tests/disagreement_finder/disagreement_with_sj_cao/inf_more-hermit_cao.clusters-of-orthologous-groups-cog-analysis-ontology.3.orig.owl");
+		paths.add("/home/michael/experiments/spot_tests/disagreement_finder/disagreement_with_sj_cao/inf_pellet_cao.clusters-of-orthologous-groups-cog-analysis-ontology.3.orig.owl");
+		OWLOntologyManager ontom = OWLManager.createOWLOntologyManager();
+		OWLOntology ontology;
+		try {
+			ontology = ontom.loadOntologyFromOntologyDocument(new File("/home/michael/Downloads/website_iswc2015/ontologies_w_problems/cao.clusters-of-orthologous-groups-cog-analysis-ontology.3.orig.owl"));
+			ArrayList<String> list = disf.getDifferences(paths, path,ontology);
+			for(String s:list)
+			{
+				System.out.println(s);
+			}
+		} catch (OWLOntologyCreationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	
 	
